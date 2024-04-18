6543bfa9ed7b0e5c61b1223cad57312b95a028a0
Binhong Wang <binhong.wang@joynexy.com>
2024-04-18 13:48:46 +0800

    update build.sh

--- release/build.sh
+++ release/build.sh
@@ -1,10 +1,8 @@
-#!/bin/bash
-
 # Function to build for a specific OS/architecture
 build_for_os_arch() {
     os=$1
     arch=$2
-    output_dir="bin/${os}-${arch}"
+    output_dir="../bin/${os}-${arch}"
 
     echo "Building for ${os}/${arch}"
 
@@ -12,11 +10,11 @@ build_for_os_arch() {
     mkdir -p "${output_dir}"
 
     # Build the kwcli executable with appropriate GOOS and GOARCH
-    GOOS="${os}" GOARCH="${arch}" go build -o "${output_dir}/kwcli" ./cmd/...
+    env CGO_ENABLED=0  GOOS="${os}" GOARCH="${arch}" go build -ldflags "-s -w"  -o "${output_dir}/kwcli" ../main.go
 }
 
 # Target OS/Architecture combinations
-os_arch_list=("linux-amd64" "darwin-amd64" "windows-amd64") # Add more as needed
+os_arch_list=("linux-amd64" "windows-amd64") # Add more as needed
 
 # Build for each target
 for target in "${os_arch_list[@]}"; do
